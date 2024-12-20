a6109b5ffee7cc973eb035e8cb8c531f9c21ce17
satyarajasree <satyanarayanarft@gmail.com>
2024-12-20 15:47:20 +0530

    Update ShiftController.java

--- src/main/java/com/rajasreeit/backend/controller/ShiftController.java
+++ src/main/java/com/rajasreeit/backend/controller/ShiftController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/crm/admin")
-@CrossOrigin(origins = "https://crm-admin-website-1wpy4o1sd-satyarajasrees-projects.vercel.app/")
+@CrossOrigin(origins = "https://crm-admin-website.vercel.app")
 public class ShiftController {
 
     @Autowired
