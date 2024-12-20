44c73acdcaf96a110608d0b3adab453c06d079e9
satyarajasree <satyanarayanarft@gmail.com>
2024-12-20 14:11:00 +0530

    Update EnquiryController.java

--- src/main/java/com/rajasreeit/backend/controller/crmControllers/EnquiryController.java
+++ src/main/java/com/rajasreeit/backend/controller/crmControllers/EnquiryController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/crm/employee")
+@CrossOrigin(origins = "https://crm-admin-website-1wpy4o1sd-satyarajasrees-projects.vercel.app/")
 public class EnquiryController {
 
     @Autowired
